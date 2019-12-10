a772559a394f940c1942f811c29fc20396052afb
Lee Booth <boothy060509@gmail.com>
2019-12-10 10:14:52 +0000

    Only apply sorting if there is more than one item to migrate.

--- src/services/GatherContent_GatherContentService.php
+++ src/services/GatherContent_GatherContentService.php
@@ -851,10 +851,11 @@ class GatherContent_GatherContentService extends Component
         $items = $this->getTemplateItems($items);
         $result = [];
 
-        usort($items, function ($item1, $item2) {
-            return $item2['id'] <=> $item1['id'];
-        });
-
+        if(count($items) > 1){
+            usort($items, function ($item1, $item2) {
+                return $item2['id'] <=> $item1['id'];
+            });
+        }
 
         if ($specificIdList !== null) {
             foreach ($items as $key => $item) {
